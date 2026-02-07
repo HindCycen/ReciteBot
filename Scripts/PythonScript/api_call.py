1955a5796d45e64a75db517d98ccda9aae47072e
HindCycen <sigmacyc@outlook.com>
2026-02-07 14:52:24 +0800

    feat: add a python file for calling api

---
+++ Scripts/PythonScript/api_call.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+
+
+# TODO: REMOVE api_key before committing!
+client = OpenAI(api_key = "", base_url = "https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+  ],
+    max_tokens=1024,
+    temperature=0.7,
+    stream=False
+)
+
+print(response.choices[0].message.content)
